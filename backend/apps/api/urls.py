98c49735204711055a9f6b2c445827d44dc23558
shiozaki <shiozaki.tomoki2@gmail.com>
2025-08-10 17:46:27 +0900

    feat(api): implement TodoListCreate api

--- backend/apps/api/urls.py
+++ backend/apps/api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from apps.api import views
 
 urlpatterns = [
-    path("todos/", views.TodoList.as_view()),
+    path("todos/", views.TodoListCreate.as_view()),
 ]
--- backend/apps/api/views.py
+++ backend/apps/api/views.py
@@ -6,7 +6,7 @@ from apps.todo.models import Todo
 
 
 # Create your views here.
-class TodoList(generics.ListAPIView):
+class TodoListCreate(generics.ListCreateAPIView):
     # ListAPIView requires two mandatory attributes, serializer_class and
     # queryset.
     # We specify TodoSerializer which we have earlier implemented
@@ -15,3 +15,7 @@ class TodoList(generics.ListAPIView):
     def get_queryset(self):
         user = self.request.user
         return Todo.objects.filter(user=user).order_by("-created")
+
+    def perform_create(self, serializer):
+        # serializer holds a django model
+        serializer.save(user=self.request.user)
