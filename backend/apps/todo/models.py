dc1580f69527e0a8f53017e0443c5baeb9ecb581
shiozaki <shiozaki.tomoki2@gmail.com>
2025-08-10 16:57:31 +0900

    feat(todo): add Todo model

--- backend/apps/todo/models.py
+++ backend/apps/todo/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+User = get_user_model()
+
+
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    memo = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
