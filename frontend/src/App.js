93d259ba6933ff479f41bf3cb5a34abece966b35
shiozaki <shiozaki.tomoki2@gmail.com>
2025-09-08 20:39:34 +0900

    refactor(Footer): move footer section to Footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import { AuthProvider } from "./context/AuthContext";
 import { ErrorProvider } from "./context/ErrorContext";
 import AppNavbar from "./components/Navbar";
 import ErrorAlert from "./components/ErrorAlert";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -18,28 +19,7 @@ function App() {
             <ErrorAlert />
             <AppRoutes />
           </Container>
-          <footer className="text-center text-lg-start bg-light text-muted mt-4">
-            <div className="text-center p-4">
-              © Copyright -{" "}
-              <a
-                target="_blank"
-                rel="noreferrer"
-                className="text-reset fw-bold text-decoration-none"
-                href="https://twitter.com/greglim81"
-              >
-                Greg Lim
-              </a>{" "}
-              -{" "}
-              <a
-                target="_blank"
-                rel="noreferrer"
-                className="text-reset fw-bold text-decoration-none"
-                href="https://twitter.com/danielgarax"
-              >
-                Daniel Correa
-              </a>
-            </div>
-          </footer>
+          <Footer />
         </AuthProvider>
       </ErrorProvider>
     </div>
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="text-center bg-light text-muted mt-4 py-3">
+    <small>
+      &copy; 2025{" "}
+      <a
+        href="https://github.com/tomoki-shiozaki"
+        target="_blank"
+        rel="noreferrer"
+        className="text-reset text-decoration-none fw-bold"
+      >
+        Tomoki Shiozaki
+      </a>
+      . All rights reserved.
+    </small>
+  </footer>
+);
+
+export default Footer;
