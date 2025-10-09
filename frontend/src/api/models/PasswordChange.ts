ebf6132d8e70aa48150b713687a341f7c7063e03
shiozaki <shiozaki.tomoki2@gmail.com>
2025-10-09 13:38:21 +0900

    chore(frontend): minor improvement

--- frontend/src/api/models/PasswordChange.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type PasswordChange = {
-    new_password1: string;
-    new_password2: string;
-};
-
