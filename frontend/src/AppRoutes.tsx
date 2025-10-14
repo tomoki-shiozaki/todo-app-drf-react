1c5b9c75d434b2f01613856118e6bc2bf22bc5a6
shiozaki <shiozaki.tomoki2@gmail.com>
2025-10-14 13:38:42 +0900

    chore(AppRoutes): migrate AppRoutes to TypeScript

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,6 +1,4 @@
-import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { useAuthContext } from "./context/AuthContext";
 
 import AddTodo from "./pages/AddTodo";
 import Login from "./pages/Login";
@@ -9,19 +7,14 @@ import TodosList from "./pages/TodosList";
 import TodoDetail from "./pages/TodoDetail";
 
 function AppRoutes() {
-  const { token, currentUsername, login, signup } = useAuthContext();
-
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/todos" replace />} />
-      <Route path="/todos" element={<TodosList token={token} />} />
+      <Route path="/todos" element={<TodosList />} />
       <Route path="/todos/create" element={<AddTodo />} />
       <Route path="/todos/:id" element={<TodoDetail />} />
-      <Route
-        path="/login"
-        element={<Login login={login} user={currentUsername} />}
-      />
-      <Route path="/signup" element={<Signup signup={signup} />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 }
