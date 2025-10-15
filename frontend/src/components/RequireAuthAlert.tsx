71f92735dbe40f4e29b6b91038d7263ae3bc1ebd
shiozaki <shiozaki.tomoki2@gmail.com>
2025-10-15 11:08:00 +0900

    fix(components): use Link in RequireAuthAlert

--- frontend/src/components/RequireAuthAlert.tsx
+++ frontend/src/components/RequireAuthAlert.tsx
@@ -1,12 +1,16 @@
 import Container from "react-bootstrap/Container";
 import Alert from "react-bootstrap/Alert";
+import { Link } from "react-router-dom";
 
 export default function RequireAuthAlert() {
   return (
     <Container className="mt-4" style={{ maxWidth: "600px" }}>
       <Alert variant="warning">
         認証情報がありません。
-        <Alert.Link href="/login">ログインページへ</Alert.Link>。
+        <Alert.Link as={Link} to="/login">
+          ログインページへ
+        </Alert.Link>
+        。
       </Alert>
     </Container>
   );
