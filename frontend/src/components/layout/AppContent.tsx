e41194dca876d7c4f123eba5e022201ae07c816a
shiozaki <shiozaki.tomoki2@gmail.com>
2025-10-17 14:26:53 +0900

    feat(error): clear global errors when location changes

--- frontend/src/components/layout/AppContent.tsx
+++ frontend/src/components/layout/AppContent.tsx
@@ -2,9 +2,19 @@ import { Container } from "react-bootstrap";
 import { useAuthContext } from "../../context/AuthContext";
 import { Loading, AppNavbar, Footer, ErrorAlert } from "../common";
 import AppRoutes from "../../AppRoutes";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { useErrorContext } from "../../context/ErrorContext";
 
 const AppContent = () => {
   const { authLoading } = useAuthContext();
+  const location = useLocation();
+  const { clearError } = useErrorContext();
+
+  // ルートが変わるたびにエラーをクリア
+  useEffect(() => {
+    clearError();
+  }, [location.pathname, clearError]);
 
   if (authLoading) {
     return <Loading message="認証情報を確認中..." />;
